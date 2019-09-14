527979028a7d6fbbcc215a5f791eac3717340f58
nathanfanelli <48001451+nathanfanelli@users.noreply.github.com>
2019-09-13 21:20:58 -0500

    Add files via upload

---
+++ arrays/todo.php
@@ -0,0 +1,21 @@
+<?php
+$list[] = [
+    'title' => 'Laundry',
+    'priority' => 2,
+    'due' => " ",
+    'complete' => true;
+];
+
+$list[] = [
+    'title' => 'Clean out refrigerator',
+    'priority' => 3,
+    'due' => '07/30/2016',
+    'complete' => false;
+];
+// Multidimensional arrays allow us to create a nested array of info for each item in a list. 
+var_dump($list);
+//echo $list[0]['title'];
+var_dump($task1);
+)
+var_dump($task1);
+?>
